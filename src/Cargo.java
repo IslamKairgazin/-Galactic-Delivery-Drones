22b22cdb5e30e2b51e79e6f5741eece98c5c4275
IslamKairgazin <islam.kairgazin@narxoz.kz>
2025-02-07 22:04:48 +0500

    Cargo.java

--- src/Cargo.java
+++ src/Cargo.java
@@ -1,1 +1,23 @@
+public class Cargo {
+    private double weight;
+    private String description;
+
+    public Cargo(double weight, String description) {
+        this.weight = weight;
+        this.description = description;
+    }
+    public double getWeight() {
+        return weight;
+    }
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
 
